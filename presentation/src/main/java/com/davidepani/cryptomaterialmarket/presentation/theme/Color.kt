6d730e7c3d6e8a0a40774f5ee26b8237d492b0fd
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-07-06 18:42:08 +0200

    [presentation] added StocksDarkTheme and colors

--- presentation/src/main/java/com/davidepani/cryptomaterialmarket/presentation/theme/Color.kt
+++ presentation/src/main/java/com/davidepani/cryptomaterialmarket/presentation/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PositiveTrend = Color(0xFF65C466)
+val NegativeTrend = Color(0xFFEB4E3D)
+
+// Stocks Dark Theme
+val StocksDarkBackground = Color(0xFF1E1E1E)
+val StocksDarkPrimaryText = Color(0xFFEBEBEB)
+val StocksDarkSecondaryText = Color(0xFF808080)
+val StocksDarkTopAppBarCollapsed = Color(0xFF212121)
+val StocksDarkSelectedChip = Color(0xFF3B3B3D)
+val StocksDarkSelectedCard = Color(0xFF404043)
+val StocksDarkBackgroundTranslucent = Color(0xFF262626)
+
--- presentation/src/main/java/com/davidepani/cryptomaterialmarket/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/davidepani/cryptomaterialmarket/presentation/theme/Theme.kt
@@ -2,14 +2,14 @@ package com.davidepani.cryptomaterialmarket.presentation.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -33,11 +33,24 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val StocksDarkColorScheme = darkColorScheme(
+    background = Color.Black,
+    onBackground = StocksDarkPrimaryText,
+    surface = Color.Black,
+    surfaceVariant = StocksDarkBackgroundTranslucent,
+    onSurfaceVariant = StocksDarkPrimaryText,
+    primary = Color.Black,
+    secondary = StocksDarkBackground,
+    tertiary = StocksDarkSelectedCard,
+    onSurface = StocksDarkPrimaryText,
+    onPrimaryContainer = StocksDarkPrimaryText,
+)
+
 @Composable
 fun CryptoMaterialMarketTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,14 +58,15 @@ fun CryptoMaterialMarketTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
+        darkTheme -> StocksDarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
