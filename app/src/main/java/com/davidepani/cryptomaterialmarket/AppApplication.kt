291f81507d4a67ceb316a1645884aeb7abe08bc8
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-07-17 13:34:16 +0200

    [presentation] added Timber

--- app/src/main/java/com/davidepani/cryptomaterialmarket/AppApplication.kt
+++ app/src/main/java/com/davidepani/cryptomaterialmarket/AppApplication.kt
@@ -2,6 +2,17 @@ package com.davidepani.cryptomaterialmarket
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class AppApplication : Application()
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Timber.treeCount == 0 && BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
